c9864e55f4df9e452017c8db2d1c82deec0b4276
damjan <damjan.mlinar@students.fhnw.ch>
2024-04-25 09:58:57 +0200

    changing rainbow table

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,10 @@
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         List<String> passwds = Passwords.getFirst2000Passwd();
+        RainbowTable table = new RainbowTable(passwds);
+        System.out.println("FINAL: " + table.getEndValue("0000000"));
 
-        for (String s : passwds){
-            System.out.println(s);
-        }
-        System.out.println(passwds.size());
-
-        String password = "HelloWorld";
-        System.out.println("68e109f0f40ca72a15e05cc22786f8e6".equals(MD5Util.hashFunc(password)));
-
-        BigInteger v = new BigInteger("29c3eea3f305d6b823f562ac4be35217",16);
-        System.out.println(Reduction.reduction(v,BigInteger.valueOf(0)));
-        // RainbowTable table = new RainbowTable(passwds);
     }
 }
--- src/main/java/RainbowTable.java
+++ src/main/java/RainbowTable.java
@@ -6,16 +6,19 @@ import java.util.Map;
 public class RainbowTable {
     private Map<String,String> rainbowTable = new HashMap<>();
 
-    public RainbowTable(List<String> passwords) throws InterruptedException {
+    public RainbowTable(List<String> passwords) {
         for (String password : passwords) {
-            String hash = MD5Util.hashFunc(password);
-
-            for (int i = 0; i <= 2000; i++){
-                hash = Reduction.reduction(new BigInteger(hash,16), BigInteger.valueOf(i));
+            String hash = password;
+            for (int i = 0; i < 2000; i++){
                 hash = MD5Util.hashFunc(hash);
+                hash = Reduction.reduction(new BigInteger(hash,16), BigInteger.valueOf(i));
             }
-            rainbowTable.put(hash, password);
+            rainbowTable.put(password, hash);
         }
+    }
+
+    public String getEndValue(String startValue){
+        return rainbowTable.getOrDefault(startValue, "");
     }
 
 
